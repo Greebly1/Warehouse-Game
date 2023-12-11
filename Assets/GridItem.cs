777eb22aca1511749285d9e4fdf0ebbb2758f219
Greebly1 <dakotat103@gmail.com>
2023-12-10 20:30:24 -0800

    Crates

--- Assets/GridItem.cs
+++ Assets/GridItem.cs
@@ -17,6 +17,11 @@ public class GridItem : MonoBehaviour
 
     [SerializeField] public int x;
     [SerializeField] public int y;
+
+    [SerializeField] GameObject boxPrefab;
+
+    [SerializeField] Transform boxSpawn;
+    public bool containsBox { get; private set; } = false;
     public bool containsPlayer { get; private set; } = false;
     public LightToggle _light;
 
@@ -26,6 +31,7 @@ public class GridItem : MonoBehaviour
         {
             Grid[x,y] = this;
             gridItems.Add(this);
+            pickupReceiver.pickedUpItem += boxPickedUp;
         }
     }
 
@@ -50,5 +56,18 @@ public class GridItem : MonoBehaviour
         containsPlayer = false;
         _light.toggleLight(false);
         Debug.Log("Player stepped out of " + x + " , " + y);
+    }
+
+    public void spawnBox()
+    {
+        if (containsBox) { Debug.Log("Already contains a box"); return; } // Early out
+
+        containsBox = true;
+        Instantiate(boxPrefab, boxSpawn.position, boxSpawn.rotation);
+    }
+
+    void boxPickedUp()
+    {
+        containsBox = false;
     }
 }
